c66390c91924f82b9ffd4f352ad8f6bb5a2a60b7
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2014-11-23 14:15:00 +0900

    program jquery.elementick

---
+++ js/jquery.elementick.js
@@ -0,0 +1,40 @@
+;(function($) {
+    'use strict';
+
+    $.fn.elementick = function(option) {
+        var opts = $.extend({}, $.fn.elementick.defaults, option);
+
+        var $elm = $(this);
+
+        var $node = $elm.contents().filter(function() {
+            return this.nodeType === opts.nodeType;
+        });
+
+        function tick(i, node) {
+            var cnt = 0;
+            var origVal = node.data;
+
+            var timerId = window.setInterval(function() {
+                opts.callback(cnt, node, origVal);
+                cnt++;
+            }, opts.duration);
+        }
+
+        if(opts.position === null || opts.position === undefined) {
+            $node.each(tick);
+        } else {
+            tick(0, $node.get(opts.position));
+        }
+
+        return this;
+    };
+
+    $.fn.elementick.defaults = {
+        duration: 300,
+        nodeType: document.COMMENT_NODE,
+        callback: function(cnt, node, origVal) {
+            node.data = node.data.slice(1) + node.data[0];
+        }
+    };
+
+}(jQuery));
